ccd9f9f460c6a9da9c0f8e921a043e0de539e327
Gustav Henningsson <gustav.henningsson98@gmail.com>
2025-02-17 15:17:24 +0100

    code cleanup, compiler warnings

--- risc0_bench/methods/guest/src/main.rs
+++ risc0_bench/methods/guest/src/main.rs
@@ -1,6 +1,4 @@
 use risc0_zkvm::guest::env;
-use std::hint::black_box; //Part of rust std lib that stops the compiler from optimizing away certain things,
-                          //potentially ruining benchmarks
 use nalgebra::Matrix2;
 
 #[allow(dead_code)]
@@ -11,15 +9,13 @@ fn main() {
     // read the input
     let input: u32 = env::read();
 
-    //let result = black_box(fibonacci(black_box(input)));
-    //let result = fibonacci2(input);
     let result = adder(input);
     // write public output to the journal
     env::commit(&result);
 }
 
 
-fn fibonacci(n: u32) -> u32 {
+fn _fibonacci(n: u32) -> u32 {
     let mut a = 0;
     let mut b = 1;
     for _ in 0..n {
@@ -31,14 +27,14 @@ fn fibonacci(n: u32) -> u32 {
 }
 
 
-fn fibonacci2(n: u32) -> u64 {
+fn _fibonacci2(n: u32) -> u64 {
     Matrix2::new(1, 1, 1, 0).pow(n - 1)[(0, 0)]
 }
 
 fn adder(iterations: u32) {
     let mut sum = 0u32;
     for i in 0..iterations {
-        sum = sum.wrapping_add(i); // Simple computation to prevent optimization
+        sum = sum.wrapping_add(i); 
     }
-    std::hint::black_box(sum); // Prevents the compiler from optimizing away the loop
+    std::hint::black_box(sum); // Stops the compiler from optimising the function
 }
